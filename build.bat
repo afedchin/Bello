0abfa24f62aa393bd736dcf0c1167f6af2b26177
Anton Fedchin <afedchin@ruswizards.com>
2014-12-28 00:02:00 +0300

    Revert future change for I***** and change skin id.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 ECHO ----------------------------------------
 echo Creating Build Folder
-IF Exist _build\skin.bello rmdir _build\skin.bello /S /Q
-md _build\skin.bello\media\
+IF Exist _build\skin.bello.mod rmdir _build\skin.bello.mod /S /Q
+md _build\skin.bello.mod\media\
 
 Echo .gitignore>>exclude.txt
 Echo Thumbs.db>>exclude.txt
@@ -13,28 +13,28 @@ Echo exclude.txt>>exclude.txt
 
 ECHO ----------------------------------------
 ECHO Creating Main XBT File...
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input media -output _build\skin.bello\media\Textures.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input media -output _build\skin.bello.mod\media\Textures.xbt
 
 ECHO ----------------------------------------
 ECHO Creating themes XBT Files...
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Blue/media -output _build\skin.bello\media\Blue.xbt
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Dark/media -output _build\skin.bello\media\Dark.xbt
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Green/media -output _build\skin.bello\media\Green.xbt
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Light/media -output _build\skin.bello\media\Light.xbt
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Orange/media -output _build\skin.bello\media\Orange.xbt
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Pink/media -output _build\skin.bello\media\Pink.xbt
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Red/media -output _build\skin.bello\media\Red.xbt
-START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Yellow/media -output _build\skin.bello\media\Yellow.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Blue/media -output _build\skin.bello.mod\media\Blue.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Dark/media -output _build\skin.bello.mod\media\Dark.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Green/media -output _build\skin.bello.mod\media\Green.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Light/media -output _build\skin.bello.mod\media\Light.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Orange/media -output _build\skin.bello.mod\media\Orange.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Pink/media -output _build\skin.bello.mod\media\Pink.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Red/media -output _build\skin.bello.mod\media\Red.xbt
+START /B /WAIT tools\TexturePacker\TexturePacker -dupecheck -input themes/Yellow/media -output _build\skin.bello.mod\media\Yellow.xbt
 ECHO ----------------------------------------
 ECHO XBT Texture Files Created...
 ECHO Building Skin Directory...
 
-xcopy "720p" "_build\skin.bello\720p" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "colors" "_build\skin.bello\colors" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "extras" "_build\skin.bello\extras" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "fonts" "_build\skin.bello\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "language" "_build\skin.bello\language" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "sounds" "_build\skin.bello\sounds" /E /Q /I /Y /EXCLUDE:exclude.txt
-xcopy "*.*" "_build\skin.bello" /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "720p" "_build\skin.bello.mod\720p" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "colors" "_build\skin.bello.mod\colors" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "extras" "_build\skin.bello.mod\extras" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "fonts" "_build\skin.bello.mod\fonts" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "language" "_build\skin.bello.mod\language" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "sounds" "_build\skin.bello.mod\sounds" /E /Q /I /Y /EXCLUDE:exclude.txt
+xcopy "*.*" "_build\skin.bello.mod" /Q /I /Y /EXCLUDE:exclude.txt
 
 del exclude.txt
